6d6539c9300b3d3e89429b4fbf512b3c51db66a9
naaadjeleyc <naaadjeleyc@gmail.com>
2021-07-06 19:01:19 +0100

    First commit

--- techSupport/settings.py
+++ techSupport/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
