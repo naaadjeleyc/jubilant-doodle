093c97bc45fc31777f26687ba06ea42cd42bccfe
naaadjeleyc <naaadjeleyc@gmail.com>
2021-07-07 18:26:30 +0100

    added views

--- TechSup/views.py
+++ TechSup/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from .models import * 
 
-# Create your views here.
 def store(request):
-    context = {}
-    return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html', context)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+
+	context = {'items':items}
+	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
+	context = {}
+	return render(request, 'store/checkout.html', context)
