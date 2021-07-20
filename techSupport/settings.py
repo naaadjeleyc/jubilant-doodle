a404af9ab03c1aba80b39925546f69bf0cea61a2
naaadjeleyc <naaadjeleyc@gmail.com>
2021-07-20 22:47:03 +0100

    make it better

--- techSupport/settings.py
+++ techSupport/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-kc)j)spl5*n&*7pr0n%5o&&1dn%-z-nkdd-ri3bmkne8ef(9e$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['supporttech.herokuapp.com','127.0.0.1' ,'localhost']
+ALLOWED_HOSTS = ['techsup.herokuapp.com','127.0.0.1' ,'localhost']
 
 
 # Application definition
